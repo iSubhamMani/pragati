260f393b77fe8b4a78e559166461b7fd0477483f
Subham Mani <manisubham09@gmail.com>
2025-01-19 11:34:25 +0530

    update

--- app/(main)/payment/failure/page.tsx
+++ app/(main)/payment/failure/page.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Failure = () => {
-  return <div>Paymnet Failed</div>;
+  return (
+    <div className="max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold text-center">Payment Failed</h1>
+    </div>
+  );
 };
 
 export default Failure;
--- app/(main)/payment/success/page.tsx
+++ app/(main)/payment/success/page.tsx
@@ -1,7 +1,17 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 import React from "react";
 
 const Success = () => {
-  return <div>Payment Completed</div>;
+  return (
+    <div className="max-w-2xl mx-auto">
+      <h1 className="text-2xl font-bold text-center">Payment Successful</h1>
+      <p className="text-center mt-4">Thank you for your purchase.</p>
+      <Link href={"/purchases"}>
+        <Button className="mt-8">Go to Purchases</Button>
+      </Link>
+    </div>
+  );
 };
 
 export default Success;
